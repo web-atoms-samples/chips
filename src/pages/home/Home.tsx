6865855276001b5cc6cd7049326b313a0187b0b3
ackava <ackava@gmail.com>
2023-04-23 13:09:58 +0530

    Added chips

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,30 @@
 import XNode from "@web-atoms/core/dist/core/XNode";
 import { ContentPage } from "@web-atoms/web-controls/dist/mobile-app/MobileApp";
+import AtomChips, { Chip, Suggestion } from "@web-atoms/web-controls/dist/basic/AtomChips";
+import categories, { ICategory } from "../../model/categories";
 
 export default class HomePage extends ContentPage {
 
+    public selected: ICategory[] = [];
+
     public async init() {
+
+        this.title = "Chips Sample";
+
+
         this.renderer = <div>
-            <div text="Hello Web Atoms !!"/>
+            <AtomChips
+                items={this.selected}
+                suggestions={categories()}
+                itemRenderer={(item) => <Chip
+                    label={item.label}
+                    icon={item.icon}
+                    />}
+                suggestionRenderer={(item) => <Suggestion
+                    icon={item.icon}
+                    label={item.label}
+                    />}
+                /> 
         </div>;
     }
 
